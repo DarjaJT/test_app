260c45d906329ad190de7ca5fd949316af3aa1f4
DarjaJestin <jestin.darja@gmail.com>
2017-08-28 13:26:52 +0300

    tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,11 +6,32 @@ RSpec.describe "StaticPages", type: :feature do
 
   subject { page }
 
+  shared_examples_for "all static pages" do # Для устранения дублирования
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  it "should have the right links on the layout" do # Тест для ссылок в шаблоне.
+    visit root_path
+    click_link "About" # Клик по ссылке About переходит на страницу About
+    expect(page).to have_title(full_title('About Us')) # Проверяем title на соответствие
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
@@ -31,7 +52,7 @@ RSpec.describe "StaticPages", type: :feature do
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
   end
 
